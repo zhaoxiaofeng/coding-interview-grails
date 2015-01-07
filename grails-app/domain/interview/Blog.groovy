fe12ff2866c02f3a869d104ba960dc8c52518683
zhaoxiaofeng <447860823@qq.com>
2015-01-07 19:34:17 +0800

    second

--- grails-app/domain/interview/Blog.groovy
+++
@@ -1,8 +0,0 @@
-package interview
-
-class Blog {//为什么不叫BlogEntry呢？
-
-     String title//不一致的缩进
-    String content
-    Date dateCreatedx//最后面的x是什么意思？
-}
--- grails-app/services/interview/BlogFacadeService.groovy
+++ grails-app/services/interview/BlogFacadeService.groovy
@@ -3,33 +3,31 @@ package interview
 import com.lerss.ent.api.BlogFacade
 import com.lerss.ent.api.BlogEntryDTO
 import grails.transaction.Transactional
-import interview.Blog
+import interview.BlogEntry
 
 @Transactional
 class BlogFacadeService implements BlogFacade {
 
-    @Override
-     List<BlogEntryDTO> getRecentEntries(int n){//不正确的缩进
-     List<BlogEntryDTO> listDTO=new ArrayList<BlogEntryDTO>();
-     List<Blog> list=Blog.list(max:n);
-     for(Blog blog:list){//使用groovy风格的代码
-     listDTO.add(new BlogEntryDTO(
-                title: blog.title,
-                content: blog.content,
-                dateCreated: blog.dateCreatedx
-        )
-     
-     );
-     }
-     return listDTO;
-     }
+	@Override
+	List<BlogEntryDTO> getRecentEntries(int n){//不正确的缩进
+		List<BlogEntryDTO> listDTO=new ArrayList<BlogEntryDTO>()
+		List<BlogEntry> list=BlogEntry.list(max:n)
+		for(blogEntry in list){//使用groovy风格的代码
+			listDTO.add(new BlogEntryDTO(
+				title: blogEntry.title,
+				content: blogEntry.content,
+				dateCreated: blogEntry.createdDate
+			))
+		}
+	return listDTO
+	}
      
     @Override
-      void publish(BlogEntryDTO entry){
-      new Blog(
-                title: entry.title,
-                content: entry.content,
-                dateCreatedx: entry.dateCreated
-        ).save();
-      }
+	void publish(BlogEntryDTO entry){
+		new BlogEntry(
+			title: entry.title,
+			content: entry.content,
+			createdDate: entry.dateCreated
+		).save()
+	}
 }
