aae259361c670ee4ed4b581b5324c216a433d5bd
pootow <pootow@gmail.com>
2015-01-07 22:11:47 +0800

    review2

--- grails-app/services/interview/BlogFacadeService.groovy
+++ grails-app/services/interview/BlogFacadeService.groovy
@@ -9,19 +9,19 @@ import interview.BlogEntry
 class BlogFacadeService implements BlogFacade {
 
 	@Override
-	List<BlogEntryDTO> getRecentEntries(int n){//不正确的缩进
-		List<BlogEntryDTO> listDTO=new ArrayList<BlogEntryDTO>()
+	List<BlogEntryDTO> getRecentEntries(int n){//不正确的缩进//修正的评审注释需要去掉//大括号前需要有空格
+		List<BlogEntryDTO> listDTO=new ArrayList<BlogEntryDTO>()//等号前后需要有空格
 		List<BlogEntry> list=BlogEntry.list(max:n)
-		for(blogEntry in list){//使用groovy风格的代码
+		for(blogEntry in list){//使用groovy风格的代码//使用groovy的.each{}风格重写
 			listDTO.add(new BlogEntryDTO(
 				title: blogEntry.title,
 				content: blogEntry.content,
 				dateCreated: blogEntry.createdDate
 			))
 		}
-	return listDTO
+	return listDTO//使用groovy风格的代码
 	}
-     
+     //不一致的缩进
     @Override
 	void publish(BlogEntryDTO entry){
 		new BlogEntry(
